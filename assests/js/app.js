0bf88e2a2b0172d899808096da5c5f0ca197be13
Kavyakaranth <kavyashreekaranth@gmail.com>
2024-09-15 20:15:29 +0530

    Static page is done

--- assests/js/app.js
+++ assests/js/app.js
@@ -1,4 +1,50 @@
-const productElement = document.getElementById("product");
-productElement.addEventListener("click", function () {
-  window.location.href = "product.html";
-});
+const product = [
+  {
+    id: 1,
+    name: "Ice cream 1",
+    price: 10,
+
+    description: "A rich and creamy chocolate bar",
+    image: "https://placehold.co/350x350/blue/white?text=Ice+cream+1 ",
+  },
+  {
+    id: 2,
+    name: "Ice cream 2",
+    price: 10,
+
+    description: "A rich and creamy vanilla bar",
+    image: "https://placehold.co/350x350/blue/white?text=Ice+cream+2",
+  },
+  {
+    id: 3,
+    name: "Ice cream 3",
+    price: 10,
+
+    description: "A rich and creamy rocky road bar",
+    image: "https://placehold.co/350x350/blue/white?text=Ice+cream+3",
+  },
+  {
+    id: 4,
+    name: "Ice cream 4",
+    price: 10,
+
+    description: "A rich and creamy chocolate bar",
+    image: "https://placehold.co/350x350/blue/white?text=Ice+cream+4",
+  },
+  {
+    id: 5,
+    name: "Ice cream 5",
+    price: 10,
+
+    description: "A rich and creamy vanilla bar",
+    image: "https://placehold.co/350x350/blue/white?text=Ice+cream+5",
+  },
+  {
+    id: 6,
+    name: "Ice cream 6",
+    price: 10,
+
+    description: "A rich and creamy rocky road bar",
+    image: "https://placehold.co/350x350/blue/white?text=Ice+cream+6",
+  },
+];
